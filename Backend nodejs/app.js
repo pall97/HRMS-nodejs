21be901d86fc5e0b98822159e2ad55a26da87c9f
deepakbansal1996 <keshavbansal055@gmail.com>
2019-02-17 01:58:32 +0530

    done some backend with app.js

--- Backend nodejs/app.js
+++ Backend nodejs/app.js
@@ -13,6 +13,21 @@ useMongoClient:true
 })
 .then(()=>console.log('Mongodb Connected'))
 .catch(err =>console.log(err));
+
+//deepak start
+require('./model/project');
+const projectmod=mongoose.model('project');
+
+require('./model/users');
+const usermod=mongoose.model('users');
+
+require('./model/skills');
+const skillmod=mongoose.model('skills');
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+//deepak end
+
 //handlebars Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');  
@@ -41,7 +56,7 @@ app.get('/userhomepage', (req, res)=>{
 app.delete('/adminhomepage', (req, res)=>{
     console.log('Delete Projects or admin details');
 )};
-route for Adminhomepage DeleteApi
+//route for Adminhomepage DeleteApi
 app.delete('/adminhomepage', (req, res)=>{
 console.log('Delete Projects or admin details');
 )};*/
@@ -49,16 +64,55 @@ console.log('Delete Projects or admin details');
 //Admin can add Users(uid, password, usertype) to the User collection using this route
 app.post('/adminhomepage/adduser', (req, res)=>{
     console.log('Add User');
+    // start deepak
+    console.log(req.body);
+   const user={
+   	Username:JSON.stringify(req.body.Username),
+   	Password:JSON.stringify(req.body.Password),
+   	Usertype:JSON.stringify(req.body.Usertype)
+   }
+
+   new usermod(user)
+   .save()
+   .then(()=> console.log('done.........'))
+   .catch(err =>console.log(err));
+   //end deepak
    });
 //route for Adminhomepage Add Skills
 //Admin can create/add skills to the skill collection using this route
 app.post('/adminhomepage/addskill', (req, res)=>{
     console.log('Add skills');
+    //start deepak 
+    console.log(req.body);
+   const skill={
+    Skillsname: JSON.stringify(req.body)
+   }
+
+   new skillmod(skill)
+   .save()
+   .then(()=> console.log('done.........'))
+   .catch(err =>console.log(err));
+   //end deepak
    });
 //route for Adminhomepage Add Project
 //Admin can create/add Projects to the Project collection using this route
 app.post('/adminhomepage/addproject', (req, res)=>{
     console.log('Add Project');
+    //start deepak
+    console.log(req.body);
+   const prjt={
+   	Projectname:JSON.stringify(req.body.Projectname),
+   	Projectdesc:JSON.stringify(req.body.Projectdesc),
+   	Techstack:JSON.stringify(req.body.Techstack),
+   	Userassigned:JSON.stringify(req.body.Usersassigned)
+   }
+
+   new projectmod(prjt)
+   .save()
+   .then(()=> console.log('done.........'))
+   .catch(err =>console.log(err));
+   //end deepak
+    
    });
 //route for userhomepage updateskills
 //user can update his skill through this route
