56c36b63dc9c59e7054774917bdd56e4772c1d8e
Pallavi Verma <get.pallavi1997@gmail.com>
2019-02-20 16:20:22 +0530

    modified userhomepage.js file

--- FrontEnd/js/userhomepage.js
+++ FrontEnd/js/userhomepage.js
@@ -5,29 +5,31 @@ function showdetails(){
 				dataType: 'json', 
 				success: function(data)
 				{
-                    console.log(data);
+                    console.log(data);     
+                    
+	var datafornewskill = {
+         var name = document.getElementById("displayUsername").value;
+             var skills = document.getElementById("mytable").value;
 				 var table=document.getElementById("mytable");
 				 for (var i=0; i<data.project.length; i++){
 				 var row=table.insertRow(table.length);
-                     //console.log(data[i])
+//                     //console.log(data[i])
                  row.insertCell(0).innerHTML=i+1;
-				 row.insertCell(1).innerHTML=data.project[i].Projectname;
-				 row.insertCell(2).innerHTML=data.project[i].Projectdesc;
-                 row.insertCell(3).innerHTML=data.project[i].Techstack; 
-				 row.insertCell(2).innerHTML="<input type='button' value='Click To View' class='editbutton' onclick=\'updateskill(\""+data.project[i]._id+"\")'>"
-				 row.insertCell(3).innerHTML="<input type='button' value='Click To View' class='deletebutton' onclick=deleteskill(\""+data.project[i]._id+"\")'>"
+				 row.insertCell(1).innerHTML=data.project[i].Name;
+				 row.insertCell(2).innerHTML=data.project[i].Skills;
+//                 row.insertCell(3).innerHTML=data.project[i].Techstack; 
+//				 row.insertCell(2).innerHTML="<input type='button' value='Click To View' class='editbutton' onclick=\'updateskill(\""+data.project[i]._id+"\")'>"
+//				 row.insertCell(3).innerHTML="<input type='button' value='Click To View' class='deletebutton' onclick=deleteskill(\""+data.project[i]._id+"\")'>"
 				     }
 		      } 
 		});
 }
 
 
-function addskill(){
-	var skillname = document.getElementById("skillnameaddskill").value;
-	//var skilldesc = document.getElementById("skilldescaddskill").value;
-	var datafornewskill={
+function addskill(){ 
+	var skillname = document.getElementById("showselect").value;  
+	var datafornewskill = {
 		"Skillname": skillname
-		//"Skilldescription": skilldesc
 	}
 	$.ajax({
 		url: 'http://localhost:5000/userhomepage/addskill',
@@ -35,7 +37,7 @@ function addskill(){
         dataType: 'json',
 		data: datafornewskill,
         success: function(res)
-								{ 
+				{ 
 						alert("Skill has been added");
 						window.location="adminhomepage.html";
 								}
